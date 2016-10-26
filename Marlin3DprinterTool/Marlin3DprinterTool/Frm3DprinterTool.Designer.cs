9bb8ac2e444e7d3125000ba044c54c67a79bf055
cabbagecreek <cabbagecreek@gmail.com>
2016-10-26 12:09:31 +0200

    Install and Uninstall of Thumbnail is one button STL viewer for Windows 7.. (should work for Win 10) Prepare WIX for user controlled installation (simple/Advanced)

--- Marlin3DprinterTool/Marlin3DprinterTool/Frm3DprinterTool.Designer.cs
+++ Marlin3DprinterTool/Marlin3DprinterTool/Frm3DprinterTool.Designer.cs
@@ -36,12 +36,12 @@ namespace Marlin3DprinterTool
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMarlin3DprinterTool));
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series11 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series12 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.tabControl3DprinterTool = new System.Windows.Forms.TabControl();
             this.tabPageEndstop = new System.Windows.Forms.TabPage();
             this.grpBxZ = new System.Windows.Forms.GroupBox();
@@ -280,9 +280,7 @@ namespace Marlin3DprinterTool
             this.grpBxInstallingStlThumbnailServer = new System.Windows.Forms.GroupBox();
             this.chkBx32BitOS = new System.Windows.Forms.CheckBox();
             this.btnUnInstallStlServer = new System.Windows.Forms.Button();
-            this.btnUnRegisterStlServer = new System.Windows.Forms.Button();
             this.btnInstallStlServer = new System.Windows.Forms.Button();
-            this.btnRegisterStlServer = new System.Windows.Forms.Button();
             this.grpBxNavigation = new System.Windows.Forms.GroupBox();
             this.btnM501ReadEeprom = new System.Windows.Forms.Button();
             this.btnM500SaveEeprom = new System.Windows.Forms.Button();
@@ -3335,7 +3333,7 @@ namespace Marlin3DprinterTool
             this.grpBxAssociateStlViewer.Controls.Add(this.btnAssociateStlViewer);
             this.grpBxAssociateStlViewer.Location = new System.Drawing.Point(17, 129);
             this.grpBxAssociateStlViewer.Name = "grpBxAssociateStlViewer";
-            this.grpBxAssociateStlViewer.Size = new System.Drawing.Size(388, 100);
+            this.grpBxAssociateStlViewer.Size = new System.Drawing.Size(287, 100);
             this.grpBxAssociateStlViewer.TabIndex = 5;
             this.grpBxAssociateStlViewer.TabStop = false;
             this.grpBxAssociateStlViewer.Text = "Associate STL viewer";
@@ -3344,7 +3342,7 @@ namespace Marlin3DprinterTool
             // 
             this.btnAssociateStlViewer.Location = new System.Drawing.Point(6, 19);
             this.btnAssociateStlViewer.Name = "btnAssociateStlViewer";
-            this.btnAssociateStlViewer.Size = new System.Drawing.Size(188, 23);
+            this.btnAssociateStlViewer.Size = new System.Drawing.Size(271, 23);
             this.btnAssociateStlViewer.TabIndex = 5;
             this.btnAssociateStlViewer.Text = "STL Viewer prefered STL viewer";
             this.btnAssociateStlViewer.UseVisualStyleBackColor = true;
@@ -3354,12 +3352,10 @@ namespace Marlin3DprinterTool
             // 
             this.grpBxInstallingStlThumbnailServer.Controls.Add(this.chkBx32BitOS);
             this.grpBxInstallingStlThumbnailServer.Controls.Add(this.btnUnInstallStlServer);
-            this.grpBxInstallingStlThumbnailServer.Controls.Add(this.btnUnRegisterStlServer);
             this.grpBxInstallingStlThumbnailServer.Controls.Add(this.btnInstallStlServer);
-            this.grpBxInstallingStlThumbnailServer.Controls.Add(this.btnRegisterStlServer);
             this.grpBxInstallingStlThumbnailServer.Location = new System.Drawing.Point(17, 19);
             this.grpBxInstallingStlThumbnailServer.Name = "grpBxInstallingStlThumbnailServer";
-            this.grpBxInstallingStlThumbnailServer.Size = new System.Drawing.Size(388, 103);
+            this.grpBxInstallingStlThumbnailServer.Size = new System.Drawing.Size(287, 103);
             this.grpBxInstallingStlThumbnailServer.TabIndex = 4;
             this.grpBxInstallingStlThumbnailServer.TabStop = false;
             this.grpBxInstallingStlThumbnailServer.Text = "Installing the STL thumbnail Shell Extension";
@@ -3376,43 +3372,23 @@ namespace Marlin3DprinterTool
             // 
             // btnUnInstallStlServer
             // 
-            this.btnUnInstallStlServer.Location = new System.Drawing.Point(185, 71);
+            this.btnUnInstallStlServer.Location = new System.Drawing.Point(6, 71);
             this.btnUnInstallStlServer.Name = "btnUnInstallStlServer";
-            this.btnUnInstallStlServer.Size = new System.Drawing.Size(165, 23);
-            this.btnUnInstallStlServer.TabIndex = 2;
-            this.btnUnInstallStlServer.Text = "UnInstall Stl Thumbnail";
+            this.btnUnInstallStlServer.Size = new System.Drawing.Size(271, 23);
+            this.btnUnInstallStlServer.TabIndex = 3;
+            this.btnUnInstallStlServer.Text = "UnInstall and unregister Stl Thumbnail";
             this.btnUnInstallStlServer.UseVisualStyleBackColor = true;
-            this.btnUnInstallStlServer.Click += new System.EventHandler(this.btnUnInstallStlServer_Click);
-            // 
-            // btnUnRegisterStlServer
-            // 
-            this.btnUnRegisterStlServer.Location = new System.Drawing.Point(185, 42);
-            this.btnUnRegisterStlServer.Name = "btnUnRegisterStlServer";
-            this.btnUnRegisterStlServer.Size = new System.Drawing.Size(165, 23);
-            this.btnUnRegisterStlServer.TabIndex = 3;
-            this.btnUnRegisterStlServer.Text = "UnRegister Stl Thumbnail";
-            this.btnUnRegisterStlServer.UseVisualStyleBackColor = true;
-            this.btnUnRegisterStlServer.Click += new System.EventHandler(this.btnUnRegisterStlServer_Click);
+            this.btnUnInstallStlServer.Click += new System.EventHandler(this.btnUnRegisterStlServer_Click);
             // 
             // btnInstallStlServer
             // 
-            this.btnInstallStlServer.Location = new System.Drawing.Point(14, 42);
+            this.btnInstallStlServer.Location = new System.Drawing.Point(6, 42);
             this.btnInstallStlServer.Name = "btnInstallStlServer";
-            this.btnInstallStlServer.Size = new System.Drawing.Size(165, 23);
+            this.btnInstallStlServer.Size = new System.Drawing.Size(271, 23);
             this.btnInstallStlServer.TabIndex = 0;
-            this.btnInstallStlServer.Text = "Install Stl Thumbnail";
+            this.btnInstallStlServer.Text = "Install  and Register STL Thumbnail";
             this.btnInstallStlServer.UseVisualStyleBackColor = true;
             this.btnInstallStlServer.Click += new System.EventHandler(this.btnInstallStlServer_Click);
-            // 
-            // btnRegisterStlServer
-            // 
-            this.btnRegisterStlServer.Location = new System.Drawing.Point(14, 71);
-            this.btnRegisterStlServer.Name = "btnRegisterStlServer";
-            this.btnRegisterStlServer.Size = new System.Drawing.Size(165, 23);
-            this.btnRegisterStlServer.TabIndex = 1;
-            this.btnRegisterStlServer.Text = "Register Stl Thumbnail";
-            this.btnRegisterStlServer.UseVisualStyleBackColor = true;
-            this.btnRegisterStlServer.Click += new System.EventHandler(this.btnRegisterStlServer_Click);
             // 
             // grpBxNavigation
             // 
@@ -3660,50 +3636,50 @@ namespace Marlin3DprinterTool
             // 
             // chartTemperature
             // 
-            chartArea1.AxisX.Title = "Secunds";
-            chartArea1.AxisY.Title = "Temp";
-            chartArea1.Name = "ChartArea1";
-            this.chartTemperature.ChartAreas.Add(chartArea1);
-            legend1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F);
-            legend1.IsTextAutoFit = false;
-            legend1.Name = "Legend1";
-            this.chartTemperature.Legends.Add(legend1);
+            chartArea3.AxisX.Title = "Secunds";
+            chartArea3.AxisY.Title = "Temp";
+            chartArea3.Name = "ChartArea1";
+            this.chartTemperature.ChartAreas.Add(chartArea3);
+            legend3.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F);
+            legend3.IsTextAutoFit = false;
+            legend3.Name = "Legend1";
+            this.chartTemperature.Legends.Add(legend3);
             this.chartTemperature.Location = new System.Drawing.Point(830, 414);
             this.chartTemperature.Name = "chartTemperature";
-            series1.BorderWidth = 2;
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series1.Color = System.Drawing.Color.Red;
-            series1.IsVisibleInLegend = false;
-            series1.Legend = "Legend1";
-            series1.Name = "Extruder";
-            series2.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
-            series2.BorderWidth = 2;
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series2.Color = System.Drawing.Color.LightCoral;
-            series2.IsVisibleInLegend = false;
-            series2.Legend = "Legend1";
-            series2.Name = "SetExtruder";
-            series3.BorderWidth = 2;
-            series3.ChartArea = "ChartArea1";
-            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series3.Color = System.Drawing.Color.Blue;
-            series3.IsVisibleInLegend = false;
-            series3.Legend = "Legend1";
-            series3.Name = "Bed";
-            series4.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
-            series4.BorderWidth = 2;
-            series4.ChartArea = "ChartArea1";
-            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series4.Color = System.Drawing.Color.LightBlue;
-            series4.IsVisibleInLegend = false;
-            series4.Legend = "Legend1";
-            series4.Name = "SetBed";
-            this.chartTemperature.Series.Add(series1);
-            this.chartTemperature.Series.Add(series2);
-            this.chartTemperature.Series.Add(series3);
-            this.chartTemperature.Series.Add(series4);
+            series9.BorderWidth = 2;
+            series9.ChartArea = "ChartArea1";
+            series9.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series9.Color = System.Drawing.Color.Red;
+            series9.IsVisibleInLegend = false;
+            series9.Legend = "Legend1";
+            series9.Name = "Extruder";
+            series10.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            series10.BorderWidth = 2;
+            series10.ChartArea = "ChartArea1";
+            series10.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series10.Color = System.Drawing.Color.LightCoral;
+            series10.IsVisibleInLegend = false;
+            series10.Legend = "Legend1";
+            series10.Name = "SetExtruder";
+            series11.BorderWidth = 2;
+            series11.ChartArea = "ChartArea1";
+            series11.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series11.Color = System.Drawing.Color.Blue;
+            series11.IsVisibleInLegend = false;
+            series11.Legend = "Legend1";
+            series11.Name = "Bed";
+            series12.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            series12.BorderWidth = 2;
+            series12.ChartArea = "ChartArea1";
+            series12.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series12.Color = System.Drawing.Color.LightBlue;
+            series12.IsVisibleInLegend = false;
+            series12.Legend = "Legend1";
+            series12.Name = "SetBed";
+            this.chartTemperature.Series.Add(series9);
+            this.chartTemperature.Series.Add(series10);
+            this.chartTemperature.Series.Add(series11);
+            this.chartTemperature.Series.Add(series12);
             this.chartTemperature.Size = new System.Drawing.Size(484, 338);
             this.chartTemperature.TabIndex = 21;
             this.chartTemperature.Text = "chart1";
@@ -4145,9 +4121,7 @@ namespace Marlin3DprinterTool
         private GroupBox grpBxInstallingStlThumbnailServer;
         private CheckBox chkBx32BitOS;
         private Button btnUnInstallStlServer;
-        private Button btnUnRegisterStlServer;
         private Button btnInstallStlServer;
-        private Button btnRegisterStlServer;
         private GroupBox grpBxAssociateStlViewer;
         private Button btnAssociateStlViewer;
     }
