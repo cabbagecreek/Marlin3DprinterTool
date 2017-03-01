f3bf271eaa50ffcb0c79701855fc35af0a0cafb1
cabbagecreek <cabbagecreek@gmail.com>
2017-03-01 22:03:15 +0100

    Hotfix for PayPal that dont start..
    Created a WebBrowser based on Chrome and all languages

---
+++ Marlin3DprinterTool/PayPalWebBrowser/FrmPayPal.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace PayPalWebBrowser
+{
+    partial class FrmPayPal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPayPal));
+            this.SuspendLayout();
+            // 
+            // FrmPayPal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(773, 325);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmPayPal";
+            this.Text = "PayPal Donation to Marlin3DprinterTool";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.FrmPayPal_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
