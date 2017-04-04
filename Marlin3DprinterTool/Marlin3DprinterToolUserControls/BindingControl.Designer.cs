6eeb474bb9c94288da5d2fbdb12e421afadd5436
Johnny Lindén <cabbagecreek@gmail.com>
2017-04-04 11:43:29 +0200

    Update of documantation

---
+++ Marlin3DprinterTool/Marlin3DprinterToolUserControls/BindingControl.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Marlin3DprinterToolUserControls
+{
+    partial class BindingControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chartBinding = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chartBinding)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartBinding
+            // 
+            chartArea1.AxisX.Minimum = 1D;
+            chartArea1.Name = "ChartArea1";
+            this.chartBinding.ChartAreas.Add(chartArea1);
+            this.chartBinding.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.chartBinding.Legends.Add(legend1);
+            this.chartBinding.Location = new System.Drawing.Point(0, 0);
+            this.chartBinding.Name = "chartBinding";
+            series1.BorderWidth = 2;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.LegendText = "Front Left Corner";
+            series1.Name = "FrontLeftCorner";
+            series2.BorderWidth = 2;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.LegendText = "Front Right Coner";
+            series2.Name = "FrontRightCorner";
+            series3.BorderWidth = 2;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.LegendText = "Back Left Coner";
+            series3.Name = "BackLeftCorner";
+            series4.BorderWidth = 2;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend1";
+            series4.LegendText = "Back Right Coner";
+            series4.Name = "BackRightCorner";
+            this.chartBinding.Series.Add(series1);
+            this.chartBinding.Series.Add(series2);
+            this.chartBinding.Series.Add(series3);
+            this.chartBinding.Series.Add(series4);
+            this.chartBinding.Size = new System.Drawing.Size(498, 319);
+            this.chartBinding.TabIndex = 0;
+            this.chartBinding.Text = "chart1";
+            // 
+            // BindingControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.chartBinding);
+            this.Name = "BindingControl";
+            this.Size = new System.Drawing.Size(498, 319);
+            ((System.ComponentModel.ISupportInitialize)(this.chartBinding)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartBinding;
+    }
+}
---
+++ Marlin3DprinterTool/Marlin3DprinterToolUserControls/BindingControl.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Marlin3DprinterToolUserControls
+{
+    public partial class BindingControl : UserControl
+    {
+        private int _frontLeftCorner;
+        private int _frontRightCorner;
+        private int _backLeftCorner;
+        private int _backRightCorner;
+
+
+        public BindingControl()
+        {
+            InitializeComponent();
+        }
+
+        public void Clear()
+        {
+            _frontLeftCorner = 0;
+            _frontRightCorner = 0;
+            _backLeftCorner = 0;
+            _backRightCorner = 0;
+            chartBinding.Series["FrontLeftCorner"].Points.Clear();
+            chartBinding.Series["FrontRightCorner"].Points.Clear();
+            chartBinding.Series["BackLeftCorner"].Points.Clear();
+            chartBinding.Series["BackRightCorner"].Points.Clear();
+            Rescale();
+        }
+
+        public void FrontLeftCorner(decimal y)
+        {
+            chartBinding.Series["FrontLeftCorner"].Points.AddXY(_frontLeftCorner, y);
+            Rescale();
+        }
+        public void FrontRightCorner(decimal y)
+        {
+            chartBinding.Series["FrontRightCorner"].Points.AddXY(_frontRightCorner, y);
+            Rescale();
+        }
+        public void BackLeftCorner(decimal y)
+        {
+            chartBinding.Series["BackRightCorner"].Points.AddXY(_backRightCorner, y);
+            Rescale();
+
+        }
+        public void BackRightCorner(decimal y)
+        {
+            chartBinding.Series["BackLeftCorner"].Points.AddXY(_backLeftCorner, y);
+            Rescale();
+        }
+
+        private void Rescale()
+        {
+            double max = 0;
+            double min = 1000;
+
+
+            for (int serieIndex = 0; serieIndex < chartBinding.Series.Count; serieIndex++)
+            {
+
+
+                foreach (DataPoint dp in chartBinding.Series[serieIndex].Points)
+                {
+                    min = Math.Min(min, dp.YValues[0]);
+                    max = Math.Max(max, dp.YValues[0]);
+
+                }
+            }
+
+            chartBinding.ChartAreas[0].AxisY.Maximum = max + (0.11 * max);
+            chartBinding.ChartAreas[0].AxisY.Minimum = (Math.Max(min - (0.1 * min), 0));
+        }
+    }
+}
