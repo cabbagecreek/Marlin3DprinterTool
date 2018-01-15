8a0b002c99ca23c2ee658e15e4d7c5e2b00ec65b
DESKTOP-APK415N\Johnnyboy <cabbagecreek@gmail.com>
2018-01-15 10:13:51 +0100

    Update of map

--- Marlin3DprinterTool/AutoUpdater/MapInfo.cs
+++ Marlin3DprinterTool/AutoUpdater/MapInfo.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Xml;
 
 namespace AutoUpdater
@@ -41,40 +43,124 @@ namespace AutoUpdater
                 url = $"http://freegeoip.net/xml/{myIp}";
                 WebRequest getMyLocationRequest = WebRequest.Create(url);
                 WebResponse getMyLocationResponse = getMyLocationRequest.GetResponse();
-                StreamReader getMyLocationStreamReader = new StreamReader(getMyLocationResponse.GetResponseStream());
+                StreamReader getMyLocationStreamReader = new StreamReader(getMyLocationResponse.GetResponseStream() ?? throw new InvalidOperationException());
 
                 xml.LoadXml(getMyLocationStreamReader.ReadToEnd().Trim());
 
                 XmlNode xmlNodeLatitude = xml.SelectSingleNode(@"/Response/Latitude");
                 XmlNode xmlNodeLongitude = xml.SelectSingleNode(@"/Response/Longitude");
-                XmlNode xmlNodeCity = xml.SelectSingleNode(@"/Response/City");
-                XmlNode xmlNodeCountryName = xml.SelectSingleNode(@"/Response/CountryName");
+                
+
+
 
 
                 MarkerPoint markerPoint = new MarkerPoint();
-                if (xmlNodeLatitude != null)
+
+                Updater updater = new Updater();
+                
+                bool isDonator = updater.IsDonator();
+
+                if (xmlNodeLatitude != null && xmlNodeLongitude != null)
                 {
-                    markerPoint.Latitude = xmlNodeLatitude.InnerText;
+                    markerPoint = 
+                         GetMyLocation(xmlNodeLatitude.InnerText, xmlNodeLongitude.InnerText, isDonator);
+                    return markerPoint;
                 }
-                if (xmlNodeLongitude != null)
-                {
-                    markerPoint.Longitude = xmlNodeLongitude.InnerText;
-                }
-                if (xmlNodeCity != null)
-                {
-                    markerPoint.City = xmlNodeCity.InnerText;
-                }
-                if (xmlNodeCountryName != null)
-                {
-                    markerPoint.CountryName = xmlNodeCountryName.InnerText;
-                }
-                return markerPoint;
+                
+                return null;
 
             }
             catch (Exception)
             {
                 return null;
             }
+
+        }
+
+        private MarkerPoint GetMyLocation(string lat, string lng, bool isDonator)
+        {
+            string coordinate = $"{lat},{lng}";
+            XmlDocument xDoc = new XmlDocument();
+
+
+            try
+            {
+                xDoc.Load("https://maps.googleapis.com/maps/api/geocode/xml?latlng=" + coordinate + "&location_type=ROOFTOP&result_type=street_address&key=AIzaSyDfNL99sHWyLZu_Fpwu4-erhQgymdH5EFI");
+
+
+                XmlNodeList xNodelst = xDoc.GetElementsByTagName("result");
+                XmlNode xNode = xNodelst.Item(0);
+
+                if (xNode != null)
+                {
+                    MarkerPoint marker = new MarkerPoint();
+                    marker.City = GetCity(xNode);
+                    marker.CountryName = GetCountry(xNode);
+                    marker.Latitude = lat;
+                    marker.Longitude = lng;
+                    marker.IsDonator = isDonator;
+                    return marker;
+                }
+
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(lat + " " + lng + ":" + e.Message);
+
+                Thread.Sleep(1000);
+            }
+
+
+
+
+
+            return null;
+        }
+
+
+        private string GetCity(XmlNode xNode)
+        {
+            string city = null;
+            XmlNode roadNode = xNode.SelectSingleNode("address_component[2]/long_name");
+            if (roadNode != null)
+            {
+                city += roadNode.InnerText + " ";
+            }
+
+            XmlNode townNode = xNode.SelectSingleNode("address_component[3]/long_name");
+            if (townNode != null)
+            {
+                city += townNode.InnerText + " ";
+            }
+
+            XmlNode areaNode = xNode.SelectSingleNode("address_component[4]/long_name");
+            if (areaNode != null)
+            {
+                city += areaNode.InnerText + " ";
+            }
+
+            XmlNode countryNode = xNode.SelectSingleNode("address_component[6]/long_name");
+            if (countryNode != null)
+            {
+                city += countryNode.InnerText + " ";
+            }
+
+            return city;
+
+        }
+
+        private string GetCountry(XmlNode xNode)
+        {
+            string country = null;
+
+
+            XmlNode countryNode = xNode.SelectSingleNode("address_component[6]/long_name");
+            if (countryNode != null)
+            {
+                country += countryNode.InnerText + " ";
+            }
+
+            return country;
 
         }
 
@@ -117,7 +203,7 @@ namespace AutoUpdater
                                 
                                           
                 string sql = "INSERT INTO xvom_wpgmza(map_id,address,description,pic,link,icon,lat,lng,anim,title,infoopen,category,approved,retina,type,did,other_data) " +
-                             $"VALUES(1,'{markerPoint.City}','','','','','{markerPoint.Latitude}','{markerPoint.Longitude}',0,'',0,{category},0,0,'','','')";
+                             $"VALUES(1,'{markerPoint.City}','','','','','{markerPoint.Latitude}','{markerPoint.Longitude}',0,'',0,{category},1,0,'','','')";
 
 
 
